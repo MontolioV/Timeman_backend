2ba7aadadbc15f66f42895e868adf279078aa51f
MontolioV <montoliov@gmail.com>
2019-01-10 17:40:58 +0200

    dev.json config for development, prod.json config must be available in production environment. Auth0 authentication. CORS.

---
+++ README.md
@@ -0,0 +1,16 @@
+# Timeman_backend
+
+Node RESTful backend for Timeman project. 
+
+## Build Setup
+
+``` bash
+# install dependencies
+$ npm install
+
+# development
+$ npm run dev
+
+# production, you must create /config/prod.js with prodution configs
+$ npm run prod
+```
--- server.js
+++ server.js
@@ -1,28 +1,50 @@
 const express = require("express");
-const rs = express();
+const app = express();
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const config = require('config');
 const user = require('./core/user/routes');
+const jwt = require('express-jwt');
+const jwks = require('jwks-rsa');
 
 console.log(`Connecting to db ${config.DBHost}`);
 mongoose.connect(config.DBHost, config.DBOptions);
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 
-rs.use(bodyParser.json());
-rs.use(bodyParser.urlencoded({extended: true}));
-rs.use(bodyParser.text());
-rs.use(bodyParser.json({type: 'application/json'}));
+const jwtCheck = jwt({
+    secret: jwks.expressJwtSecret({
+        cache: true,
+        rateLimit: true,
+        jwksRequestsPerMinute: 5,
+        jwksUri: config.jwksUri
+    }),
+    audience: config.jwtAudience,
+    issuer: config.jwtIssuer,
+    algorithms: config.jwtAlgorithms
+});
 
-rs.get('/', (req, res) => res.json({message: 'Connection success, waiting for requests'}));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.text());
+app.use(bodyParser.json({type: 'application/json'}));
+// CORS control
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", config.CORSAllowedDomain);
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
-rs.route('/users')
+app.get('/', (req, res) => res.json({message: 'Connection success, waiting for requests'}));
+
+app.route('/users')
     .get(user.getUsers)
+    // .get(jwtCheck, user.getUsers)
     .post(user.postUser);
-rs.route('/:login')
+app.route('/:login')
     .get(user.getUser);
 
-rs.listen(config.ServerPort, function (req, res) {
+app.listen(config.ServerPort, function (req, res) {
     console.log(`REST server started and listening port ${config.ServerPort}`);
 });
+
